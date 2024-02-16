d4272c05b83c259488e7520446c53eebe3665a1b
= <=>
2024-02-16 16:14:21 +0100

    the lazy way

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("The codebeez Python 1B row challenge")
+import time
+
+import duckdb
+import pandas as pd
+
+
+def the_lazy_way():
+    # 144 sec :(
+    start_time = time.time()
+    df = pd.read_csv(
+        "data/measurements.txt", delimiter=";", header=None, names=["station", "measurement"]
+    )
+    df = duckdb.query(
+        "SELECT station, ROUND(MIN(measurement),1) as _min, ROUND(AVG(measurement),1) as _avg, ROUND(MAX(measurement),1) as _max, FROM df GROUP BY station"
+    )
+    print(df.to_dict(orient="records"))
+    end = time.time() - start_time
+    print(f"duration: {end} sec")
 
 
 if __name__ == "__main__":
-    main()
+    the_lazy_way()
